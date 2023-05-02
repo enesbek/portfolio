05092690385b96c71a5e81772629969ab19e04f2
Enes Bek <enesbek@Enes-MacBook-Air.local>
2023-05-02 14:50:10 +0300

    feat: texts and images updated

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -50,7 +50,7 @@ const articles = () => {
       <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light">
         <Layout className="pt-16">
           <AnimatedText
-            text="Lorem Ipsum Dolor"
+            text="Exploring Frontend Development"
             className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:text-4xl"
           />
           <ul className="grid grid-cols-2 gap-16 md:grid-cols-1 lg:gap-8 md:gap-y-16">
